e25a24e398fb4da161c02d22de1a57e887e813c4
Joe Liberty <joe@codewest.net>
2015-05-06 13:44:31 +1000

    Adding gulp dependancies.

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+/* File: gulpfile.js */
+
+// grab our gulp packages
+var gulp  = require('gulp'),
+    gutil = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+// create a default task and just log a message
+gulp.task('default', function() {
+  return gutil.log('Gulp is running!')
+});
+
+
+// define the default task and add the watch task to it
+// gulp.task('default', ['watch']);
+
+// configure the jshint task
+gulp.task('jshint', function() {
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+  gulp.watch('source/javascript/**/*.js', ['jshint']);
+  gulp.watch('source/scss/**/*.scss', ['build-css']);
+});
+
+gulp.task('build-css', function() {
+  return gulp.src('source/scss/**/*.scss')
+    .pipe(sourcemaps.init())  // Process the original sources
+      .pipe(sass())
+    .pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(gulp.dest('public/assets/stylesheets'));
+});
+
+gulp.task('build-js', function() {
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('bundle.js'))
+      //only uglify if gulp is ran with '--type production'
+      .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) 
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/assets/javascript'));
+});
